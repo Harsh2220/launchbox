404ccd9c8854483146dcb78006f02c52d51c301b
sahilkakwani9 <sahkakwani@gmail.com>
2024-10-05 20:18:16 +0530

    fix: migrate

--- src/app/transfer/[id]/page.tsx
+++ src/app/transfer/[id]/page.tsx
@@ -1,26 +1,22 @@
 "use client";
 import getConfig from "@/utils/api/getConfig";
 import WormholeConnect from "@wormhole-foundation/wormhole-connect";
+import { useParams, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
-
 function Transfer() {
-
   const [config, setConfig] = useState(null);
+  const params = useParams();
+
+  const { id } = useRouter<{ id: string }>();
 
   useEffect(() => {
-    getConfig("0c41f2f7-614e-4c3c-8bba-3ff5b8bcc1d3").then(data => {
+    getConfig(id).then((data) => {
       setConfig(data);
-    })
-  }, [])
+    });
+  }, []);
 
-  return (
-    <div>
-      {
-        config && <WormholeConnect config={config} />
-      }
-    </div>
-  );
+  return <div>{config && <WormholeConnect config={config} />}</div>;
 }
 
 export default Transfer;
--- src/utils/api/registerMigration.ts
+++ src/utils/api/registerMigration.ts
@@ -1,8 +1,25 @@
-export default async function registerMigration(request: RegisterNewTokenRequest) {
-    const res = await fetch("/api/register-migration", {
+export default async function registerMigration(
+  request: RegisterMigrationRequest
+) {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/registerMigration`,
+      {
         method: "POST",
-        body: JSON.stringify(request)
-    })
-    const data = await res.json()
-    return data
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(request),
+      }
+    );
+
+    if (!res.ok) {
+      throw new Error(`Error: ${res.statusText}`);
+    }
+    const json = await res.json();
+
+    return json;
+  } catch (error) {
+    console.log(error);
+  }
 }
