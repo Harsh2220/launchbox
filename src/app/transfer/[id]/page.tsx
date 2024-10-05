f018b708f628d5cf6f6c245f6e224402d43f98ff
Harsh <harshgajjar145@gmail.com>
2024-10-05 22:54:13 +0530

    chore: footer changes

--- src/app/transfer/[id]/page.tsx
+++ src/app/transfer/[id]/page.tsx
@@ -1,13 +1,14 @@
 "use client";
+
 import getConfig from "@/utils/api/getConfig";
 import WormholeConnect from "@wormhole-foundation/wormhole-connect";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
-function Transfer() {
+export default function Transfer() {
   const [config, setConfig] = useState(null);
   const { id } = useParams();
-  console.log(id);
+
   useEffect(() => {
     if (!id) return;
     getConfig(id as string).then((data) => {
@@ -17,5 +18,3 @@ function Transfer() {
 
   return <div>{config && <WormholeConnect config={config} />}</div>;
 }
-
-export default Transfer;
