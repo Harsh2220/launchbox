c23cd957755dc74a2dbe9aa3285904a98bf0583b
sahilkakwani9 <sahkakwani@gmail.com>
2024-10-06 19:34:19 +0530

    fix: next config cors

--- next.config.mjs
+++ next.config.mjs
@@ -7,9 +7,28 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   poweredByHeader: false,
-  webpack: (config) => {
+  webpack: (config, { isServer, dev }) => {
+    if (!isServer && !dev) {
+      config.optimization.splitChunks.cacheGroups = {
+        ...config.optimization.splitChunks.cacheGroups,
+        defaultVendors: false,
+      };
+    }
     config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
+  },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: `frame-ancestors 'self' https://*.walletconnect.org https://*.walletconnect.com;`,
+          },
+        ],
+      },
+    ];
   },
 };
 
